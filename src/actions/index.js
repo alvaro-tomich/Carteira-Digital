060a6f7f01f20961c41fc0a93c84f989009de601
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-03-06 08:33:06 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+const saveEmail = (email) => ({
+  type: 'SAVE_EMAIL',
+  payload: {
+    email,
+  },
+});
+
+export default saveEmail;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import saveEmail from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.name);
+    if (event.target.name === 'email') {
+      this.setState({
+        email: event.target.value,
+      });
+    }
+    if (event.target.name === 'password') {
+      this.setState({
+        password: event.target.value,
+      });
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { email } = this.state;
+    const { addEmailToGlobalStage, history } = this.props;
+    addEmailToGlobalStage(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const minPasswordCharacters = 6;
+    const { email, password } = this.state;
+    const emailCheck = /^[a-z-0-9]+@[a-z]+.[a-z]+$/.test(email);
+    const passwordCheck = password.length < minPasswordCharacters;
+    const isDisabled = passwordCheck || !emailCheck;
+
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <label htmlFor="email-input">
+          <input
+            name="email"
+            type="email"
+            data-testid="email-input"
+            placeholder="Digite seu email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            name="password"
+            type="password"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ isDisabled }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addEmailToGlobalStage: (email) => (
+    dispatch(saveEmail(email))
+  ),
+});
+
+Login.propTypes = {
+  addEmailToGlobalStage: PropTypes.func.isRequired,
+  history: PropTypes.string.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
