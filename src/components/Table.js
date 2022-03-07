c72b9c400adab1ea1671d0326f596816e71b3e53
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-03-07 17:14:00 -0300

    Adicionando thead na tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
